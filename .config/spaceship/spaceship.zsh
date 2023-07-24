30abbc2c91fae4c93ea62f978072542b9de6ab5c
Collin Brittain <collinbrittain@gmail.com>
2023-07-24 10:10:10 -0500

    Tweaking prompt

--- .config/spaceship/spaceship.zsh
+++ .config/spaceship/spaceship.zsh
@@ -2,5 +2,16 @@
 # Display time
 SPACESHIP_TIME_SHOW=true
 
-# Do not truncate path in repos
+# Dir section
 SPACESHIP_DIR_TRUNC_REPO=false
+SPACESHIP_DIR_PREFIX=""
+
+# Remove unecessary prefixes
+SPACESHIP_GIT_PREFIX=""
+SPACESHIP_PACKAGE_PREFIX=""
+
+# Remove some default sections
+spaceship remove user
+spaceship remove golang
+spaceship remove rust
+spaceship remove docker
